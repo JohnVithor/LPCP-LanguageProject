7ae782deb44487d1511c36ec69ab764d05bf2ef5
1danielsc <danielscolao@gmail.com>
2022-06-27 20:19:16 -0300

    Adicionado eval para soma e concatenação

--- src/Eval.hs
+++ src/Eval.hs
@@ -2,6 +2,15 @@ module Eval where
 import Lexer
 import Type
 
+
 eval :: Type -> Token -> Type -> Type
 eval (Type.String x) (Plus _ ) (Type.String y) = Type.String (x ++ y)
+eval (Type.Int x) (Plus _ ) (Type.Int y _) = Type.Int (x + y) 
+eval (Type.Real x) (Plus _ ) (Type.Real y _) = Type.Real (x + y) 
+eval (Type.Int x) (Plus _ ) (Type.Real y _) = Type.Real (x + y) 
+eval (Type.Real x) (Plus _ ) (Type.Int y _) = Type.Real (x + y) 
+eval (Type.Char x) (Plus _ ) (Type.Char y _) = Type.String ([x] ++ [y]) 
+eval (Type.String x) (Plus _ ) (Type.Char y _) = Type.String (x ++ [y]) 
+eval (Type.Char x) (Plus _ ) (Type.String y _) = Type.String ([x] ++ y) 
+
 eval _ _ _ = error "deu ruim"
