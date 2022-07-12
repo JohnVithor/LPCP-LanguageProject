f714b7159d09e8ab9421ed9ed9608b1877b63e6e
Daniel Sehn Colao <66954777+1DanielSC@users.noreply.github.com>
2022-07-12 12:29:51 -0300

    wip: atualiza implementações

---
+++ Eval.hs
@@ -0,0 +1,137 @@
+module Eval where
+import Lexer
+import Type
+import Text.Read
+import SymTable
+
+cast :: Token -> Type -> Type
+cast (Lexer.CastingReal _) (Type.Int value) = Type.Real (fromIntegral value)
+cast (Lexer.CastingReal _) (Type.Real value) = Type.Real value
+cast (Lexer.CastingReal _) (Type.String value) = do
+    let v = readMaybe value
+    maybe (error ("Não é possível converter '" ++ value ++ "' para Real")) Type.Real v
+
+cast (Lexer.CastingInt _) (Type.Int value) = Type.Int value
+cast (Lexer.CastingInt _) (Type.Real value) = Type.Int (truncate value)
+cast (Lexer.CastingInt _) (Type.String value) = do
+    let v = readMaybe value
+    maybe (error ("Não é possível converter '" ++ value ++ "' para Int")) Type.Int v
+
+cast a b = error ("cast inválido: " ++ show a ++ show b)
+
+
+eval :: Type -> Token -> Type -> Type
+eval (Type.String x) (Plus _ ) (Type.String y) = Type.String (x ++ y)
+eval (Type.Char x) (Plus _ ) (Type.Char y ) = Type.String (x : [y])
+eval (Type.String x) (Plus _ ) (Type.Char y ) = Type.String (x ++ [y])
+eval (Type.Char x) (Plus _ ) (Type.String y ) = Type.String (x : y)
+eval (Type.Int x) (Plus _ ) (Type.Int y) = Type.Int (x + y)
+eval (Type.Real x) (Plus _ ) (Type.Real y) = Type.Real (x + y)
+eval (Type.Int x) (Plus _ ) (Type.Real y) = Type.Real (fromIntegral x + y)
+eval (Type.Real x) (Plus _ ) (Type.Int y) = Type.Real (x + fromIntegral y)
+eval (Type.Int x) (Minus _ ) (Type.Int y) = Type.Int (x - y)
+eval (Type.Real x) (Minus _ ) (Type.Real y) = Type.Real (x - y)
+eval (Type.Int x) (Minus _ ) (Type.Real y) = Type.Real (fromIntegral x - y)
+eval (Type.Real x) (Minus _ ) (Type.Int y) = Type.Real (x - fromIntegral y)
+eval (Type.Int x) (Mult _ ) (Type.Int y) = Type.Int (x * y)
+eval (Type.Real x) (Mult _ ) (Type.Real y) = Type.Real (x * y)
+eval (Type.Int x) (Mult _ ) (Type.Real y) = Type.Real (fromIntegral x * y)
+eval (Type.Real x) (Mult _ ) (Type.Int y) = Type.Real (x * fromIntegral y)
+eval (Type.Int x) (Div _ ) (Type.Int y) = Type.Int (x `div` y)
+eval (Type.Real x) (Div _ ) (Type.Real y) = Type.Real (x / y)
+eval (Type.Int x) (Div _ ) (Type.Real y) = Type.Real (fromIntegral x / y)
+eval (Type.Real x) (Div _ ) (Type.Int y) = Type.Real (x / fromIntegral y)
+eval (Type.Int x) (Mod _ ) (Type.Int y) = Type.Int (x `mod` y)
+eval (Type.Bool x) (And _ ) (Type.Bool y) = Type.Bool (x && y)
+eval (Type.Bool x) (Or _ ) (Type.Bool y) = Type.Bool (x || y)
+eval (Type.Int x) (LessOrEqual _ ) (Type.Int y) = Type.Bool (x <= y)
+eval (Type.Real x) (LessOrEqual _ ) (Type.Real y) = Type.Bool (x <= y)
+eval (Type.Int x) (LessOrEqual _ ) (Type.Real y) = Type.Bool (fromIntegral x <= y)
+eval (Type.Real x) (LessOrEqual _ ) (Type.Int y) = Type.Bool (x <= fromIntegral y)
+eval (Type.Int x) (GreaterOrEqual _ ) (Type.Int y) = Type.Bool (x >= y)
+eval (Type.Real x) (GreaterOrEqual _ ) (Type.Real y) = Type.Bool (x >= y)
+eval (Type.Int x) (GreaterOrEqual _ ) (Type.Real y) = Type.Bool (fromIntegral x >= y)
+eval (Type.Real x) (GreaterOrEqual _ ) (Type.Int y) = Type.Bool (x >= fromIntegral y)
+eval (Type.Int x) (Equal _ ) (Type.Int y) = Type.Bool (x == y)
+eval (Type.Real x) (Equal _ ) (Type.Real y) = Type.Bool (x == y)
+eval (Type.Int x) (Equal _ ) (Type.Real y) = Type.Bool (fromIntegral x == y)
+eval (Type.Real x) (Equal _ ) (Type.Int y) = Type.Bool (x == fromIntegral y)
+eval (Type.String x) (Equal _ ) (Type.String y) = Type.Bool (x == y)
+eval (Type.Char x) (Equal _ ) (Type.Char y) = Type.Bool (x == y)
+eval (Type.Int x) (NotEqual _ ) (Type.Int y) = Type.Bool (x /= y)
+eval (Type.Real x) (NotEqual _ ) (Type.Real y) = Type.Bool (x /= y)
+eval (Type.Int x) (NotEqual _ ) (Type.Real y) = Type.Bool (fromIntegral x /= y)
+eval (Type.Real x) (NotEqual _ ) (Type.Int y) = Type.Bool (x /= fromIntegral y)
+eval (Type.Char x) (NotEqual _ ) (Type.Char y) = Type.Bool (x /= y)
+eval (Type.String x) (NotEqual _ ) (Type.String y) = Type.Bool (x /= y)
+eval (Type.Int x) (Less _ ) (Type.Int y) = Type.Bool (x < y)
+eval (Type.Real x) (Less _ ) (Type.Real y) = Type.Bool (x < y)
+eval (Type.Int x) (Less _ ) (Type.Real y) = Type.Bool (fromIntegral x < y)
+eval (Type.Real x) (Less _ ) (Type.Int y) = Type.Bool (x < fromIntegral y)
+eval (Type.Int x) (Greater _ ) (Type.Int y) = Type.Bool (x > y)
+eval (Type.Real x) (Greater _ ) (Type.Real y) = Type.Bool (x > y)
+eval (Type.Int x) (Greater _ ) (Type.Real y) = Type.Bool (fromIntegral x > y)
+eval (Type.Real x) (Greater _ ) (Type.Int y) = Type.Bool (x > fromIntegral y)
+eval t1 op t2 = error ("A operação " ++ show op ++" não está definida para os tipos " ++ show t1 ++" e " ++ show t2)
+
+
+evalUni :: Token -> Type -> Type
+evalUni (Not _ ) (Type.Bool y) = Type.Bool (not y)
+evalUni (Minus _ ) (Type.Int y) = Type.Int (-y)
+evalUni (Minus _ ) (Type.Real y) = Type.Real (-y)
+evalUni op t = error ("A operação " ++ show op ++" não está definida para o tipo " ++ show t)
+
+getStmts :: Subprogram -> [Token]
+getStmts (_, _, _, stmts) = stmts
+
+getArgs :: Subprogram -> [(String, Type)]
+getArgs (_, _, args, _) = args
+
+
+
+
+-- Eval para acessar alguma posicao de uma array (1d)]
+evalArrayAcess :: [Type] -> Type  -> Type
+evalArrayAcess ([Type.Int list])  (Type.Int index)  = Type.Int (accessArray index list)
+evalArrayAcess ([Type.Real list])  (Type.Int index)  = Type.Real (accessArray index list)
+evalArrayAcess ([Type.Bool list])  (Type.Int index)  = Type.Bool (accessArray index list)
+evalArrayAcess ([Type.String list])  (Type.Int index)  = Type.String (accessArray index list)
+evalArrayAcess list type_index = error ("Indice não é um inteiro")
+
+
+
+-- Eval para acessar alguma posicao de uma matriz (2d)
+eval2dArrayAcess :: [Type] ->  Type ->  Type -> Type  -> Type
+eval2dArrayAcess ([Type.Int list])  (Type.Int rowIndex)   (Type.Int columnIndex) (Type.Int numRows)  = Type.Int (accessArray (columnIndex + rowIndex*numRows) list)
+eval2dArrayAcess ([Type.Real list])  (Type.Int rowIndex)   (Type.Int columnIndex) (Type.Int numRows)  = Type.Real (accessArray (columnIndex + rowIndex*numRows) list)
+eval2dArrayAcess ([Type.Bool list])  (Type.Int rowIndex)   (Type.Int columnIndex) (Type.Int numRows)  = Type.Bool (accessArray (columnIndex + rowIndex*numRows) list)
+eval2dArrayAcess ([Type.String list])  (Type.Int rowIndex)   (Type.Int columnIndex) (Type.Int numRows)  = Type.String (accessArray (columnIndex + rowIndex*numRows) list)
+eval2dArrayAcess list type_index_row type_index_col  = error ("Indice não é um inteiro")
+
+
+--Eval para criar array (1d)
+evalCreateArray :: Type -> Type  -> Type
+evalCreateArray t (Type.Int length) = Type.List length 1 (createArray length t)
+
+
+--Eval para criar matriz (array 2d)
+evalCreateMatrix :: Type -> Type -> Type  -> Type
+evalCreateMatrix t (Type.Int numRows) (Type.Int numCols) = Type.List numRows numCols (createArray numRows*numCols t)
+
+--Eval para acessar alguma posicao da lista (array 1d ou array 2d)
+accessArray :: Int -> [Type] -> Type
+accessArray index (x:xs) 
+                        | index > 0 = (accessArray (index-1) xs)
+                        | index == 0 = x
+                        | otherwise = error ("Access out of bounds!") --Some error message
+
+--Eval para criar um array (1d ou 2d)
+createArray :: Int -> Type -> [Type]
+createArray length v = replicate length v
+
+--Eval para atribuir um valor a alguma posicao da array (1d ou 2d)
+assignValueArray :: Int -> Type -> [Type] -> [Type]
+assignValueArray index value (x:xs)
+                                | index > 0 = x:(assignValue (index-1) value xs)
+                                | index < 0 = error ("Access out of bounds!") --Some error message
+                                | otherwise = value:xs
---
+++ Type.hs
@@ -0,0 +1,85 @@
+module Type where
+
+data Type = Bool Bool                       | 
+            Int Int                         |
+            Real Double                     | 
+            Char Char                       | 
+            String String                   |
+            List Int Int  [Type]                |
+            Struct String [(String, Type)]  |
+            Ref String String               
+            deriving (Eq,Show)
+
+getTypeName :: Type -> String
+getTypeName (Type.Bool _) = "bool"
+getTypeName (Type.Int _) = "int"
+getTypeName (Type.Real _) = "real"
+getTypeName (Type.Char _) = "char"
+getTypeName (Type.String _) = "string"
+getTypeName (Type.List _ _) = "list"
+getTypeName (Type.Struct name _) = name
+getTypeName (Type.Ref _ _) = "ref"
+
+getStructName :: Type -> String
+getStructName (Type.Struct name _) = name
+getStructName _ = error "Not a Struct"
+
+getStructField :: Type -> String -> Type
+getStructField (Type.Struct _ values) field = getStructFieldInner values field
+-- getStructField (Type.Ref (Type.Struct _ values) _) field = getStructFieldInner values field
+getStructField _ _ = error "Not a Struct"
+
+printVal :: Type -> IO()
+printVal (Type.Bool v) = putStr (show v)
+printVal (Type.Int v) = putStr (show v)
+printVal (Type.Real v) = putStr (show v)
+printVal (Type.Char v) = putStr (show v)
+printVal (Type.String v) = putStr (show v)
+printVal (Type.List _ v) = putStr (show v)
+printVal (Type.Struct name _) =  putStr name
+printVal (Type.Ref v ref) = putStr (ref++"["++show v++"]")
+
+printVals ::[(String, Type)] -> IO()
+printVals ((_,v):vs) = do
+    putStr ","
+    printVal v
+    printVals vs
+printVals [] = putStr ""
+
+
+getStructFieldInner :: [(String, Type)] -> String -> Type
+getStructFieldInner [] _ = error "deu ruim"
+getStructFieldInner ((name, value):values) field 
+    | field == name = value
+    | otherwise = getStructFieldInner values field
+
+
+compatible :: Type -> Type -> Bool
+compatible (Type.Int _) (Type.Int _) = True
+compatible (Type.Real _) (Type.Real _) = True
+compatible (Type.String _) (Type.String _) = True
+compatible (Type.Char _) (Type.Char _) = True
+compatible (Type.Bool _) (Type.Bool _) = True
+compatible (Type.Struct name1 _) (Type.Struct name2 _) = name1 == name2
+compatible (Type.Ref x1 _) (Type.Ref x2 _) = x1 == x2
+compatible _ _ = False
+
+ifRefOf :: Type -> Type -> Bool 
+ifRefOf (Type.Ref x _) t = x == getTypeName t
+ifRefOf _ _ = False
+
+isRefType :: Type -> Bool 
+isRefType (Type.Ref _ _) = True
+isRefType _ = False
+
+getRefKey :: Type -> String
+getRefKey (Type.Ref _ key) = key
+getRefKey _ = ""
+
+-- setRefValue :: Type -> Type -> Type
+-- setRefValue (Type.Ref _ n) v = Type.Ref v n
+-- setRefValue a _ = error ("Não é do Tipo Ref!: " ++ show a)
+
+-- getRefValue :: Type -> Type
+-- getRefValue (Type.Ref v _) = v
+-- getRefValue a = error ("Não é do Tipo Ref!: " ++ show a)
